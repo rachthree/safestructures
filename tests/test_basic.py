21f9e5b562ca5439862b5ed151bd04c19c894577
Craig Chan <46288912+rachthree@users.noreply.github.com>
2024-11-10 21:37:03 -0500

    Simplify tests

--- tests/test_basic.py
+++ tests/test_basic.py
@@ -21,65 +21,69 @@ numeric_test_cases = [
 ]
 
 
-class TestBasicProcessor:
-    """Test class for the basic datatype processor."""
+def test_serialize_str(serializer):
+    """Test string serialization to schema."""
+    test_input = "mock_input"
+    expected_value = test_input
+    result = BasicProcessor(serializer).serialize(test_input)
 
-    def test_serialize_str(self, serializer):
-        """Test string serialization to schema."""
-        test_input = "mock_input"
-        expected_value = test_input
-        result = BasicProcessor(serializer).serialize(test_input)
+    check_schema(result, "str", expected_value)
 
-        check_schema(result, "str", expected_value)
 
-    def test_deserialize_str(self, serializer):
-        """Test deserialization to string."""
-        test_value = "mock_input"
-        test_schema = generate_schema("str", test_value)
-        result = BasicProcessor(serializer).deserialize(test_schema)
-        assert result == test_value
+def test_deserialize_str(serializer):
+    """Test deserialization to string."""
+    test_value = "mock_input"
+    test_schema = generate_schema("str", test_value)
+    result = BasicProcessor(serializer).deserialize(test_schema)
+    assert result == test_value
 
-    @pytest.mark.parametrize("test_input,expected_type", numeric_test_cases)
-    def test_serialize_numeric(self, serializer, test_input, expected_type):
-        """Test numeric serialization to schema."""
-        expected_value = str(test_input)
-        result = BasicProcessor(serializer).serialize(test_input)
-        check_schema(result, expected_type, expected_value)
 
-    @pytest.mark.parametrize("expected_value,schema_type", numeric_test_cases)
-    def test_deserialize_numeric(self, serializer, expected_value, schema_type):
-        """Test deserialization to numeric."""
-        schema_input = str(expected_value)
-        test_schema = generate_schema(schema_type, schema_input)
-        result = BasicProcessor(serializer).deserialize(test_schema)
-        assert result == expected_value
+@pytest.mark.parametrize("test_input,expected_type", numeric_test_cases)
+def test_serialize_numeric(serializer, test_input, expected_type):
+    """Test numeric serialization to schema."""
+    expected_value = str(test_input)
+    result = BasicProcessor(serializer).serialize(test_input)
+    check_schema(result, expected_type, expected_value)
 
-    @pytest.mark.parametrize("test_input", [True, False])
-    def test_serialize_bool(self, serializer, test_input):
-        """Test boolean serialization to schema."""
-        expected_value = test_input
-        result = BasicProcessor(serializer).serialize(test_input)
-        check_schema(result, "bool", expected_value)
 
-    @pytest.mark.parametrize("test_input", [True, False])
-    def test_deserialize_bool(self, serializer, test_input):
-        """Test deserialization to boolean."""
-        expected_value = test_input
-        test_schema = generate_schema("bool", test_input)
-        result = BasicProcessor(serializer).deserialize(test_schema)
-        assert result == expected_value
+@pytest.mark.parametrize("expected_value,schema_type", numeric_test_cases)
+def test_deserialize_numeric(serializer, expected_value, schema_type):
+    """Test deserialization to numeric."""
+    schema_input = str(expected_value)
+    test_schema = generate_schema(schema_type, schema_input)
+    result = BasicProcessor(serializer).deserialize(test_schema)
+    assert result == expected_value
 
-    def test_serialize_null(self, serializer):
-        """Test null serialization to schema."""
-        test_input = None
-        expected_value = test_input
-        result = BasicProcessor(serializer).serialize(test_input)
 
-        check_schema(result, "NoneType", expected_value)
+@pytest.mark.parametrize("test_input", [True, False])
+def test_serialize_bool(serializer, test_input):
+    """Test boolean serialization to schema."""
+    expected_value = test_input
+    result = BasicProcessor(serializer).serialize(test_input)
+    check_schema(result, "bool", expected_value)
 
-    def test_deserialize_null(self, serializer):
-        """Test deserialization to None."""
-        test_value = None
-        test_schema = generate_schema("NoneType", test_value)
-        result = BasicProcessor(serializer).deserialize(test_schema)
-        assert result is None
+
+@pytest.mark.parametrize("test_input", [True, False])
+def test_deserialize_bool(serializer, test_input):
+    """Test deserialization to boolean."""
+    expected_value = test_input
+    test_schema = generate_schema("bool", test_input)
+    result = BasicProcessor(serializer).deserialize(test_schema)
+    assert result == expected_value
+
+
+def test_serialize_null(serializer):
+    """Test null serialization to schema."""
+    test_input = None
+    expected_value = test_input
+    result = BasicProcessor(serializer).serialize(test_input)
+
+    check_schema(result, "NoneType", expected_value)
+
+
+def test_deserialize_null(serializer):
+    """Test deserialization to None."""
+    test_value = None
+    test_schema = generate_schema("NoneType", test_value)
+    result = BasicProcessor(serializer).deserialize(test_schema)
+    assert result is None
