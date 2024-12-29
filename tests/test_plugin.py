91114c1f6abce9d54da724efd85671b7b7baff18
Craig Chan <46288912+rachthree@users.noreply.github.com>
2024-12-29 00:31:58 -0500

    Test multiple plugins

--- tests/test_plugin.py
+++ tests/test_plugin.py
@@ -2,24 +2,26 @@
 
 import torch
 from transformers import BertConfig, BertModel
-from transformers.modeling_outputs import BaseModelOutputWithPoolingAndCrossAttentions
+from transformers.modeling_outputs import (
+    BaseModelOutputWithPastAndCrossAttentions,
+    BaseModelOutputWithPoolingAndCrossAttentions,
+    ModelOutput,
+)
 from utils import compare_values
 
 from safestructures import load_file, save_file
 from safestructures.processors.iterable import DataclassProcessor
 
 
-class BertModelOutputProcessor(DataclassProcessor):
-    """Processor for BaseModelOutputWithPoolingAndCrossAttentions."""
+class ModelOutputProcessor(DataclassProcessor):
+    """Processor for `transformers`'s ModelOutput."""
 
-    data_type = BaseModelOutputWithPoolingAndCrossAttentions
+    data_type = ModelOutput
 
-    def deserialize(
-        self, serialized: dict, **kwargs
-    ) -> BaseModelOutputWithPoolingAndCrossAttentions:
+    def deserialize(self, serialized: dict, **kwargs) -> ModelOutput:
         """Overload DataclassProcessor.deserialize.
 
-        This is so the proper BERT model output is provided.
+        This is so the proper ModelOutput is provided.
         """
         mo_kwargs = {}
         for k, v in serialized.items():
@@ -30,17 +32,47 @@ class BertModelOutputProcessor(DataclassProcessor):
         return model_output_instance
 
 
+class BertOutputProcessor(ModelOutputProcessor):
+    """Processor for BERT model outputs."""
+
+    data_type = BaseModelOutputWithPoolingAndCrossAttentions
+
+
+class BertEncoderOutputProcessor(ModelOutputProcessor):
+    """Processor for BERT encoder outputs."""
+
+    data_type = BaseModelOutputWithPastAndCrossAttentions
+
+
 def test_transformers_plugin(tmp_path):
     """Test an example plugin."""
     config = BertConfig()
     model = BertModel(config)
+    test_plugins = [BertOutputProcessor, BertEncoderOutputProcessor]
+
+    results = {}
+
+    def _store_encoder_output(module, args, kwargs, output):
+        results["encoder_output"] = output
+        return output
+
+    model.encoder.register_forward_hook(_store_encoder_output, with_kwargs=True)
+
     test_input_ids = torch.tensor([[0] * 128])
     test_output = model(test_input_ids)
 
+    results["model_output"] = test_output
+
     test_filepath = tmp_path / "test.safestructures"
-    save_file(test_output, test_filepath, plugins=[BertModelOutputProcessor])
+    save_file(results, test_filepath, plugins=test_plugins)
 
-    deserialized = load_file(test_filepath, plugins=[BertModelOutputProcessor])
-    assert isinstance(deserialized, BaseModelOutputWithPoolingAndCrossAttentions)
+    deserialized = load_file(test_filepath, plugins=test_plugins)
+    assert isinstance(
+        deserialized["encoder_output"], BaseModelOutputWithPastAndCrossAttentions
+    )
+    assert isinstance(
+        deserialized["model_output"], BaseModelOutputWithPoolingAndCrossAttentions
+    )
 
-    compare_values(test_output, deserialized)
+    compare_values(results["encoder_output"], deserialized["encoder_output"])
+    compare_values(results["model_output"], deserialized["model_output"])
